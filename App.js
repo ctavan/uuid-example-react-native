7a33fe40d04e426a07a0d99ea2fda379784bf770
Christoph Tavan <dev@tavan.de>
2020-09-09 11:52:52 +0200

    Upgrade react native

--- App.js
+++ App.js
@@ -7,9 +7,16 @@
  */
 
 import React from 'react';
-import { SafeAreaView, StyleSheet, ScrollView, View, Text, StatusBar } from 'react-native';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
 
-import { v1 as uuidv1, v4 as uuidv4, v3 as uuidv3, v5 as uuidv5 } from 'uuid';
+import {v1 as uuidv1, v4 as uuidv4, v3 as uuidv3, v5 as uuidv5} from 'uuid';
 
 const App: () => React$Node = () => {
   const MY_NAMESPACE = '55238d15-c926-4598-b49d-cf4e913ba13c';
@@ -18,7 +25,9 @@ const App: () => React$Node = () => {
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
-        <ScrollView contentInsetAdjustmentBehavior="automatic" style={styles.scrollView}>
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={styles.scrollView}>
           <View style={styles.body}>
             <View style={styles.sectionContainer}>
               <Text style={styles.sectionTitle}>uuidv1()</Text>
@@ -54,12 +63,17 @@ const App: () => React$Node = () => {
             </View>
             <View style={styles.sectionContainer}>
               <Text style={styles.sectionTitle}>uuidv3() MY_NAMESPACE</Text>
-              <Text style={styles.sectionDescription}>... using a custom namespace</Text>
               <Text style={styles.sectionDescription}>
-                Note: Custom namespaces should be a UUID string specific to your application! E.g.
-                the one here was generated using this modules `uuid` CLI.
+                ... using a custom namespace
               </Text>
-              <Text style={styles.sectionDescription}>{uuidv3('Hello, World!', MY_NAMESPACE)}</Text>
+              <Text style={styles.sectionDescription}>
+                Note: Custom namespaces should be a UUID string specific to your
+                application! E.g. the one here was generated using this modules
+                `uuid` CLI.
+              </Text>
+              <Text style={styles.sectionDescription}>
+                {uuidv3('Hello, World!', MY_NAMESPACE)}
+              </Text>
             </View>
             <View style={styles.sectionContainer}>
               <Text style={styles.sectionTitle}>uuidv5() DNS</Text>
@@ -81,12 +95,17 @@ const App: () => React$Node = () => {
             </View>
             <View style={styles.sectionContainer}>
               <Text style={styles.sectionTitle}>uuidv5() MY_NAMESPACE</Text>
-              <Text style={styles.sectionDescription}>... using a custom namespace</Text>
               <Text style={styles.sectionDescription}>
-                Note: Custom namespaces should be a UUID string specific to your application! E.g.
-                the one here was generated using this modules `uuid` CLI.
+                ... using a custom namespace
               </Text>
-              <Text style={styles.sectionDescription}>{uuidv5('Hello, World!', MY_NAMESPACE)}</Text>
+              <Text style={styles.sectionDescription}>
+                Note: Custom namespaces should be a UUID string specific to your
+                application! E.g. the one here was generated using this modules
+                `uuid` CLI.
+              </Text>
+              <Text style={styles.sectionDescription}>
+                {uuidv5('Hello, World!', MY_NAMESPACE)}
+              </Text>
             </View>
           </View>
         </ScrollView>
